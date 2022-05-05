010c80941fae899991b318dbbdbd20026165371f
Kaley White <kwhite31415@gmail.com>
2022-05-05 11:13:57 -0500

    Document name_matches

--- RayStation/exclude_from_msq_export.py
+++ RayStation/exclude_from_msq_export.py
@@ -9,99 +9,122 @@ clr.AddReference('System.Windows.Forms')
 from System.Windows.Forms import MessageBox
 
 
-def name_matches(base, name):
-    regex = base + r' \(\d+\)'
+def name_matches(base: str, name: str) -> bool:
+    """Determines whether a name matches the given name (case insensitive), possibly with a copy number in parentheses
+
+    Arguments
+    ---------
+    base: The "base name" (without a copy number) to compare the name to
+    name: The name to compare to the "base" name
+
+    Returns:
+    True if the name exactly matches the base or is the base plus a copy number, False otherwise
+
+    Examples
+    --------
+    name_matches("SpinalCord", "Spinalcord (1)") -> True
+    name_matches("SpinalCord", "Spinal Cord") -> False
+    name_matches("SpinalCord", "Spinal Cord (1)") -> False
+    """
+    regex = base + r'( \(\d+\))?'
     m = re.match(regex, name, re.IGNORECASE)
     return m is not None
 
 
-def exclude_from_mosaiq_export() -> None:
-    """Includes/excludes the current case's ROIs from export
-    
-    Makes visible all included ROIs and makes invisible all excluded ROIs
+def hi_max_dose(dose_dist, geom):
+    """Determines whether Dmax (0.035) for the geometry in the dose distribution is at least 10 Gy
 
-    Includes and makes visible:
-    - SpinalCord
-    - Targets
-    - Support ROIs
-    - External
-    - If a plan is open and has dose:
-        - Any otherwise excluded ROIs with max dose >= 10 Gy in the current plan
+    Arguments
+    ---------
+    dose_dist: The dose distribution that the geometry belongs to
+    geom: The ROI geometry
 
-    Exclude and make invisible:
-    - ROI named "box" (case insensitive)
-    - Control ROIs
-    - If a plan is open:
-        - ROIs with empty geometries on planning exam of current plan
-        - If the current plan has dose:
-            - Any otherwise included ROIs with max dose < 10 Gy in the current plan
-
-    Assumes all ROI names are TG-263 compliant
-    
-    Upon script completion, the user should review the included/excluded ROIs in ROI/POI Details and make any necessary changes
+    Returns
+    -------
+    True if D0.035 >= 10 Gy, False otherwise
     """
-    # Get current variables
+    if dose_dist is None:
+        return False
+    rel_vol = 0.035 / geom.GetRoiVolume()
+    max_dose = dose_dist.GetDoseAtRelativeVolumes(RoiName=geom.OfRoi.Name, RelativeVolumes=[rel_vol])[0]
+    return max_dose >= 1000
+
+
+def exclude_from_msq_export() -> None:
+    """Includes/excludes the current case's ROIs from export.
+    
+    Makes visible all included ROIs and makes invisible all excluded ROIs.
+    
+    Includes SpinalCord, Targets, External and ROIs with materials defined.
+    Included ROIs must also be nonempty (planning exam if a plan is open, ecurrent exam otherwise. If neither plan nor exam is open, ignore empty/nonempty rule).
+    If the open plan has dose, included ROIs mcut also have max dose >= 10 Gy.
+
+    Excludes "Box" and Control.
+
+    Assumes all ROI names are TG-263 compliant. ROI names with a copy number are considered matches (e.g., "SpinalCord (1)" is "SpinalCord").
+    
+    Upon script completion, the user should review the included/excluded ROIs in ROI/POI Details and make any necessary changes.
+    """
+    ## Get current variables
+    # Patient
     try:
         patient = get_current('Patient')
     except:
         MessageBox.Show('There is no patient open. Click OK to abort the script.', 'No Open Patient')
         sys.exit()
+    # Case
     try:
         case = get_current('Case')
     except:
         MessageBox.Show('There is no case open. Click OK to abort the script.', 'No Open Case')
         sys.exit()
+    # Dose distribution (for checking max geometry dose) and ROI geometries
+    dose_dist = roi_geoms = None
     try:
+        # If a plan is open, there may be dose, and use the plan's structure set
         plan = get_current('Plan')
-        dose_dist = plan.TreatmentCourse.TotalDose
-        if dose_dist.DoseValues is None:
-            dose_dist = None
-        struct_set = plan.GetTotalDoseStructureSet()
-    except:
-        dose_dist = None
-    try:
+        if plan.TreatmentCourse.TotalDose.DoseValues is not None:
+            dose_dist = plan.TreatmentCourse.TotalDose
         roi_geoms = plan.GetTotalDoseStructureSet().RoiGeometries
     except:
+        # Use current exam's structure set if an exam is open
         try:
             roi_geoms = case.PatientModel.StructureSets[get_current('Examination').Name].RoiGeometries
         except:
-            roi_geoms = None
+            pass
     
-    # Lists of OARs to include, other structures to include, and all structures to exclude
-    # Separate OARs and other structures so we can remove OAR dependencies 
-    include_organs, include_other, exclude = [], [], []  
+    # Lists of ROI names to include/exclude
+    include, exclude = [], []
     
-    for roi_name in case.PatientModel.RegionsOfInterest:
-        if name_match('box', roi_name) or roi.Type == 'Control':
-            exclude.append(roi_name)
-        elif roi_geoms is not None:
-            geom = roi_geoms[roi_name]
-
-        
-        elif  or not geom.HasContours():  # ROI is a control or geometry has no contours
-            exclude.append(roi_name)
-        elif names_match(roi_name, 'SpinalCord'):
-            include_organs.append(roi_name)
-        elif geom.OfRoi.OrganData.OrganType == 'Target' or geom.OfRoi.Type in ['Support', 'External']:
-            include_other.append(roi_name)
-        else:  # include/exclude determined by max dose @ appr vol
-            rel_vol = 0.035 / geom.GetRoiVolume()
-            max_dose = dose_dist.GetDoseAtRelativeVolumes(RoiName=roi_name, RelativeVolumes=[rel_vol])[0]
-            if max_dose >= 1000:
-                include_organs.append(roi_name)
+    # Add each ROI name to include or exclude list
+    for roi in case.PatientModel.RegionsOfInterest:
+        # Include if material override
+        if roi.RoiMaterial is not None:
+            include.append(roi.Name)
+        # Exclude "Box" or Control
+        elif name_matches('box', roi.Name) or roi.Type == 'Control':
+            exclude.append(roi.Name)
+        # Include anything else if there is no dose or structure set (so we can't check is nonempty or has high max dose)
+        elif roi_geoms is None:
+            include.append(roi.Name)
+        # Check structure set and/or dose distribution to determine whether to include
+        else:
+            geom = roi_geoms[roi.Name]
+            if geom.HasContours():  # Nonempty
+                # Include SpinalCord, target, external, if no dose (so can't check max dose), or if max dose is high
+                if name_matches('SpinalCord', roi.Name) or roi.OrganData.OrganType == 'Target' or roi.Type == 'External' or dose_dist is None or hi_max_dose(dose_dist, geom):
+                    include.append(roi.Name)
+                else:
+                    exclude.append(roi.Name)
+            # Exclude empty
             else:
-                exclude.append(roi_name)
-
-    # Remove dependent ROIs  
-    dependent_rois = [struct_set.RoiGeometries[geom_name].GetDependentRois() for geom_name in include_organs]
-    include_organs = [geom for geom in include_organs if geom not in dependent_rois]
-    include = include_organs + include_other
+                exclude.append(roi.Name)
  
     # Include/exclude from export and set visibility
     with CompositeAction('Apply ROI changes'):
         case.PatientModel.ToggleExcludeFromExport(ExcludeFromExport=False, RegionOfInterests=include)
-        for roi in include:
-            patient.SetRoiVisibility(RoiName=roi, IsVisible=True)
+        for roi_name in include:
+            patient.SetRoiVisibility(RoiName=roi_name, IsVisible=True)
         case.PatientModel.ToggleExcludeFromExport(ExcludeFromExport=True, RegionOfInterests=exclude)
-        for roi in exclude:
-            patient.SetRoiVisibility(RoiName=roi, IsVisible=False)
+        for roi_name in exclude:
+            patient.SetRoiVisibility(RoiName=roi_name, IsVisible=False)
